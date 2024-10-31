4d783b80b3d0b61b9bf28b5505284f77e02ff844
Bouchellal Imad Eddine <124831129+Imad-Bchl@users.noreply.github.com>
2024-10-31 22:05:53 +0100

    Create Colon.java

---
+++ Colon.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Colon {
+    private String nom;
+    private ArrayList<Integer> preferances;
+    private int index, ressource;
+
+    public Colon(String nom, int index) {
+        this.nom = nom;
+        this.index = index;
+        this.ressource = 0;
+        preferances = new ArrayList<Integer>();
+    }
+
+    public ArrayList<Integer> getPreferances() {
+        return preferances;
+    }
+
+    public void setPreferances(ArrayList<Integer> preferances) {
+        this.preferances = preferances;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getRessource() {
+        return ressource;
+    }
+
+    public void setRessource(int ressource) {
+        this.ressource = ressource;
+    }
+    public String getNom() {
+        return nom;
+    }
+}
